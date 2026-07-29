fd00af4065c223d146c24591fe8997bd9a6e78c8
Zerodayu <kimianalconera39@gmail.com>
2026-07-29 18:32:24 +0000

    update: remove redundant line

--- hyprland/rules.lua
+++ hyprland/rules.lua
@@ -1,6 +1,5 @@
 -- Fastfetch floating tile
-hl.window_rule({ match = { class = "^(fastfetch-float)$" }, float = true })
-hl.window_rule({ match = { class = "^(fastfetch-float)$" }, center = true })
+hl.window_rule({ match = { class = "^(fastfetch-float)$" }, float = true, center = true })
 
 hl.window_rule({ match = { class = "^()$", title = "^()$" }, no_blur = false })
 
