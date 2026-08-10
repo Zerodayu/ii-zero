2efcd04f62c9a34d7fc09b4bd27771f2a1d6ef01
Zerodayu <kimianalconera39@gmail.com>
2026-08-10 23:23:43 +0800

    new: added lazyvim discord presence for activity show

---
+++ configs/nvim-plugin/cord.lua
@@ -0,0 +1,20 @@
+return {
+	"vyfor/cord.nvim",
+	opts = {
+		editor = {
+			client = "lazyvim",
+		},
+		display = {
+			view = "full",
+			theme = "catppuccin",
+			flavor = "accent",
+		},
+		advanced = {
+			discord = {
+				reconnect = {
+					enabled = true,
+				},
+			},
+		},
+	},
+}
